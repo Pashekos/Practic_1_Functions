92ad45e3e21bee9a32adcf7e164b1e2be712d336
Pashekos <satonin.p@mail.ru>
2025-12-30 14:23:36 +0300

    Create Calculate.py

---
+++ Calculate.py
@@ -0,0 +1,34 @@
+"""
+Реализуйте программу калькулятор. 
+На вход подается три значения: первое число, второе число 
+и операция( *, /, + или -) . 
+На выходе должны получить число, после выполнения операции.
+"""
+
+import random
+
+first_number = random.randint(0,100)
+second_number = 0 #random.randint(0,100)
+list_operation =('+', '-', '/', '*')
+
+operation = random.choice(list_operation)
+
+print (first_number)
+print (operation)
+print (second_number)
+
+def print_operation(first_number, second_number, operation):
+    match operation:
+        case '+':
+            print ('Результат сложения:', first_number + second_number)
+        case '-':
+            print ('Результат вычитания:', first_number - second_number)
+        case '*':
+            print ('Результат умножения:', first_number * second_number)
+        case '/':
+            try:
+                print ('Результат деления', first_number / second_number)
+            except ZeroDivisionError:
+                print ('На ноль делить нельзя!')
+            
+print_operation(first_number, second_number, operation)
